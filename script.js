a6969e00ec34bddbe3a557878371a51d0b1f6ddb
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-01-26 23:03:12 +0300

    Testing video upload 1

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const Name = "Mostafa";
 
+let videos = [
+  "../../../../Assets/Porn/Abbey Rain - Don’t Tell My Manager!.mp4",
+  "../../../../Assets/Porn/Abbey Rain - Don’t Tell My Manager!.mp4",
+  "../../../../Assets/Porn/Abbey Rain - Don’t Tell My Manager!.mp4",
+];
+
 const Gallery = document.querySelector(".gallery");
 
 // alert("hello " + Name);
@@ -11,10 +17,7 @@ function Main() {
   this.video.muted = true;
 
   this.source1 = document.createElement("source");
-  this.source1.setAttribute(
-    "src",
-    "../../../../Assets/Porn/Abbey Rain - Don’t Tell My Manager!.mp4"
-  );
+  this.source1.setAttribute("src", "E:/Porn/1.mp4");
   this.source1.setAttribute("type", "video/mp4");
 
   this.video.classList.add("rounded-lg");
