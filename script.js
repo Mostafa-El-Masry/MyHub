6951183d0ed7d3452e0cd38f6f317ba7b1e1165b
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-01-26 22:39:01 +0300

    Adding some Assets Creating Gallery

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const Name = "Mostafa";
 
+const Gallery = document.querySelector(".gallery");
+
 // alert("hello " + Name);
+function Main() {
+  this.video = document.createElement("video");
+  this.video.controls = true;
+  this.video.autoplay = true;
+  this.video.loop = true;
+  this.video.muted = true;
+
+  this.source1 = document.createElement("source");
+  this.source1.setAttribute(
+    "src",
+    "../../../../Assets/Porn/Abbey Rain - Don’t Tell My Manager!.mp4"
+  );
+  this.source1.setAttribute("type", "video/mp4");
+
+  this.video.classList.add("rounded-lg");
+
+  this.video.appendChild(this.source1);
+
+  Gallery.appendChild(this.video);
+}
+
+var main = new Main();
